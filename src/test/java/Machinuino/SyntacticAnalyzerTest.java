5be1dc6915aaa4d838fb03ffb2fe6de3f5c5d03d
Caio Herrera <caio.herrera@outlook.com>
2017-02-03 15:16:17 -0200

    Refactoring SyntaticAnalyzerTest using JUnit4

--- src/test/java/Machinuino/SyntacticAnalyzerTest.java
+++ src/test/java/Machinuino/SyntacticAnalyzerTest.java
@@ -1,160 +1,209 @@
 package Machinuino;
 
-import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
 
-public class SyntacticAnalyzerTest extends TestCase {
+import java.io.IOException;
+
+public class SyntacticAnalyzerTest {
     private String pathNoErrors = "src/test/testCases/noErrors/";
     private String pathSyntacticErrors = "src/test/testCases/syntacticErrors/";
     private String pathLexicalErrors = "src/test/testCases/lexicalErrors/";
 
-    /* No Errors */
-    public void testNoErrors() throws Exception {
-        assertEquals("", SyntacticAnalyzer.analyzeFile(pathNoErrors + "template.moore"));
+    private void compareExpectedToResult(String expected, String fileName) {
+        String result;
+        try {
+            result = SyntacticAnalyzer.analyzeFile(fileName);
+        } catch (IOException e) {
+            Assert.fail("Failed to open file " + fileName + "!");
+            return;
+        }
+        Assert.assertEquals(expected, result);
     }
 
-    public void testCommentAfterMoore() throws Exception {
-        assertEquals("", SyntacticAnalyzer.analyzeFile(pathNoErrors + "commentAfterMoore.moore"));
+    @Test(expected = IOException.class)
+    public void inexistentFileShouldThrowException() throws IOException {
+        SyntacticAnalyzer.analyzeFile("rofl.lmao");
+    }
+
+    /* No Errors */
+    @Test
+    public void noErrors() {
+        compareExpectedToResult("", pathNoErrors + "template.moore");
+    }
+
+    @Test
+    public void commentAfterMoore() {
+        compareExpectedToResult("", pathNoErrors + "commentAfterMoore.moore");
     }
 
     /* Syntactic Errors */
-    public void testOnlyComment() throws Exception {
-        assertEquals("66: Syntactic error next to '<EOF>'" + System.lineSeparator(),
-                SyntacticAnalyzer.analyzeFile(pathSyntacticErrors + "onlyComment.moore"));
+    @Test
+    public void OnlyComment() {
+        compareExpectedToResult("66: Syntactic error next to '<EOF>'" + System.lineSeparator(),
+                pathSyntacticErrors + "onlyComment.moore");
     }
 
-    public void testMissingOpenComment() throws Exception {
-        assertEquals("2: Syntactic error next to 'The'" + System.lineSeparator(),
-                SyntacticAnalyzer.analyzeFile(pathSyntacticErrors + "missingOpenComment.moore"));
+    @Test
+    public void testMissingOpenComment() {
+        compareExpectedToResult("2: Syntactic error next to 'The'" + System.lineSeparator(),
+                pathSyntacticErrors + "missingOpenComment.moore");
     }
 
-    public void testMissingOpenBrackets() throws Exception {
-        assertEquals("3: Syntactic error next to 'states'" + System.lineSeparator(),
-                SyntacticAnalyzer.analyzeFile(pathSyntacticErrors + "missingOpenBrackets.moore"));
+    @Test
+    public void missingOpenBrackets() {
+        compareExpectedToResult("3: Syntactic error next to 'states'" + System.lineSeparator(),
+                pathSyntacticErrors + "missingOpenBrackets.moore");
     }
 
-    public void testMissingCloseBrackets() throws Exception {
-        assertEquals("3: Syntactic error next to 'states'" + System.lineSeparator(),
-                SyntacticAnalyzer.analyzeFile(pathSyntacticErrors + "missingCloseBrackets.moore"));
+    @Test
+    public void missingCloseBrackets() {
+        compareExpectedToResult("3: Syntactic error next to 'states'" + System.lineSeparator(),
+                pathSyntacticErrors + "missingCloseBrackets.moore");
     }
 
-    public void testMissingPinNumber() throws Exception {
-        assertEquals("11: Syntactic error next to ','" + System.lineSeparator(),
-                SyntacticAnalyzer.analyzeFile(pathSyntacticErrors + "missingPinNumber.moore"));
+    @Test
+    public void missingPinNumber() {
+        compareExpectedToResult("11: Syntactic error next to ','" + System.lineSeparator(),
+                pathSyntacticErrors + "missingPinNumber.moore");
     }
 
-    public void testMissingStateName() throws Exception {
-        assertEquals("18: Syntactic error next to '{'" + System.lineSeparator(),
-                SyntacticAnalyzer.analyzeFile(pathSyntacticErrors + "missingStateName.moore"));
+    @Test
+    public void missingStateName() {
+        compareExpectedToResult("18: Syntactic error next to '{'" + System.lineSeparator(),
+                pathSyntacticErrors + "missingStateName.moore");
     }
 
-    public void testMissingAmpersand() throws Exception {
-        assertEquals("19: Syntactic error next to 'switch'" + System.lineSeparator(),
-                SyntacticAnalyzer.analyzeFile(pathSyntacticErrors + "missingAmpersand.moore"));
+    @Test
+    public void missingAmpersand() {
+        compareExpectedToResult("19: Syntactic error next to 'switch'" + System.lineSeparator(),
+                pathSyntacticErrors + "missingAmpersand.moore");
     }
 
-    public void testMissingArrow() throws Exception {
-        assertEquals("19: Syntactic error next to 'q1'" + System.lineSeparator(),
-                SyntacticAnalyzer.analyzeFile(pathSyntacticErrors + "missingArrow.moore"));
+    @Test
+    public void missingArrow() {
+        compareExpectedToResult("19: Syntactic error next to 'q1'" + System.lineSeparator(),
+                pathSyntacticErrors + "missingArrow.moore");
     }
 
-    public void testMissingPinAtTransition() throws Exception {
-        assertEquals("19: Syntactic error next to '->'" + System.lineSeparator(),
-                SyntacticAnalyzer.analyzeFile(pathSyntacticErrors + "missingPinAtTransition.moore"));
+    @Test
+    public void missingPinAtTransition() {
+        compareExpectedToResult("19: Syntactic error next to '->'" + System.lineSeparator(),
+                pathSyntacticErrors + "missingPinAtTransition.moore");
     }
 
-    public void testMissingPinName() throws Exception {
-        assertEquals("43: Syntactic error next to ':'" + System.lineSeparator(),
-                SyntacticAnalyzer.analyzeFile(pathSyntacticErrors + "missingPinName.moore"));
+    @Test
+    public void missingPinName() {
+        compareExpectedToResult("43: Syntactic error next to ':'" + System.lineSeparator(),
+                pathSyntacticErrors + "missingPinName.moore");
     }
 
-    public void testMissingColon() throws Exception {
-        assertEquals("43: Syntactic error next to '5'" + System.lineSeparator(),
-                SyntacticAnalyzer.analyzeFile(pathSyntacticErrors + "missingColon.moore"));
+    @Test
+    public void missingColon() {
+        compareExpectedToResult("43: Syntactic error next to '5'" + System.lineSeparator(),
+                pathSyntacticErrors + "missingColon.moore");
     }
 
-    public void testMissingComma() throws Exception {
-        assertEquals("44: Syntactic error next to 'led'" + System.lineSeparator(),
-                SyntacticAnalyzer.analyzeFile(pathSyntacticErrors + "missingComma.moore"));
+    @Test
+    public void missingComma() {
+        compareExpectedToResult("44: Syntactic error next to 'led'" + System.lineSeparator(),
+                pathSyntacticErrors + "missingComma.moore");
     }
 
-    public void testMissingCloseComment() throws Exception {
-        assertEquals("48: Syntactic error next to 'function'" + System.lineSeparator(),
-                SyntacticAnalyzer.analyzeFile(pathSyntacticErrors + "missingCloseComment.moore"));
+    @Test
+    public void missingCloseComment() {
+        compareExpectedToResult("48: Syntactic error next to 'function'" + System.lineSeparator(),
+                pathSyntacticErrors + "missingCloseComment.moore");
     }
 
-    public void testMooreNameWrong() throws Exception {
-        assertEquals("1: Syntactic error next to 'muure'" + System.lineSeparator(),
-                SyntacticAnalyzer.analyzeFile(pathSyntacticErrors + "wrongMooreName.moore"));
+    @Test
+    public void mooreNameWrong() {
+        compareExpectedToResult("1: Syntactic error next to 'muure'" + System.lineSeparator(),
+                pathSyntacticErrors + "wrongMooreName.moore");
     }
 
-    public void testWrongStatesName() throws Exception {
-        assertEquals("3: Syntactic error next to 'staites'" + System.lineSeparator(),
-                SyntacticAnalyzer.analyzeFile(pathSyntacticErrors + "wrongStatesName.moore"));
+    @Test
+    public void wrongStatesName() {
+        compareExpectedToResult("3: Syntactic error next to 'staites'" + System.lineSeparator(),
+                pathSyntacticErrors + "wrongStatesName.moore");
     }
 
-    public void testWrongInputName() throws Exception {
-        assertEquals("7: Syntactic error next to 'innput'" + System.lineSeparator(),
-                SyntacticAnalyzer.analyzeFile(pathSyntacticErrors + "wrongInputName.moore"));
+    @Test
+    public void wrongInputName() {
+        compareExpectedToResult("7: Syntactic error next to 'innput'" + System.lineSeparator(),
+                pathSyntacticErrors + "wrongInputName.moore");
     }
 
-    public void testWrongPinsName() throws Exception {
-        assertEquals("9: Syntactic error next to 'pis'" + System.lineSeparator(),
-                SyntacticAnalyzer.analyzeFile(pathSyntacticErrors + "wrongPinsName.moore"));
+    @Test
+    public void wrongPinsName() {
+        compareExpectedToResult("9: Syntactic error next to 'pis'" + System.lineSeparator(),
+                pathSyntacticErrors + "wrongPinsName.moore");
     }
 
-    public void testWrongClockName() throws Exception {
-        assertEquals("10: Syntactic error next to 'click'" + System.lineSeparator(),
-                SyntacticAnalyzer.analyzeFile(pathSyntacticErrors + "wrongClockName.moore"));
+    @Test
+    public void wrongClockName() {
+        compareExpectedToResult("10: Syntactic error next to 'click'" + System.lineSeparator(),
+                pathSyntacticErrors + "wrongClockName.moore");
     }
 
-    public void testWrongTransitionName() throws Exception {
-        assertEquals("17: Syntactic error next to 'tranion'" + System.lineSeparator(),
-                SyntacticAnalyzer.analyzeFile(pathSyntacticErrors + "wrongTransitionName.moore"));
+    @Test
+    public void wrongTransitionName() {
+        compareExpectedToResult("17: Syntactic error next to 'tranion'" + System.lineSeparator(),
+                pathSyntacticErrors + "wrongTransitionName.moore");
     }
 
-    public void testWrongLogicalExpression() throws Exception {
-        assertEquals("19: Syntactic error next to '&'" + System.lineSeparator(),
-                SyntacticAnalyzer.analyzeFile(pathSyntacticErrors + "wrongLogicalExpression.moore"));
+    @Test
+    public void wrongLogicalExpression() {
+        compareExpectedToResult("19: Syntactic error next to '&'" + System.lineSeparator(),
+                pathSyntacticErrors + "wrongLogicalExpression.moore");
     }
 
-    public void testWrongLogicalExpression2() throws Exception {
-        assertEquals("20: Syntactic error next to '->'" + System.lineSeparator(),
-                SyntacticAnalyzer.analyzeFile(pathSyntacticErrors + "wrongLogicalExpression2.moore"));
+    @Test
+    public void wrongLogicalExpression2() {
+        compareExpectedToResult("20: Syntactic error next to '->'" + System.lineSeparator(),
+                pathSyntacticErrors + "wrongLogicalExpression2.moore");
     }
 
-    public void testWrongOutputName() throws Exception {
-        assertEquals("41: Syntactic error next to 'autput'" + System.lineSeparator(),
-                SyntacticAnalyzer.analyzeFile(pathSyntacticErrors + "wrongOutputName.moore"));
+    @Test
+    public void wrongOutputName() {
+        compareExpectedToResult("41: Syntactic error next to 'autput'" + System.lineSeparator(),
+                pathSyntacticErrors + "wrongOutputName.moore");
     }
 
-    public void testWrongPinsName2() throws Exception {
-        assertEquals("42: Syntactic error next to 'piiins'" + System.lineSeparator(),
-                SyntacticAnalyzer.analyzeFile(pathSyntacticErrors + "wrongPinsName2.moore"));
+    @Test
+    public void wrongPinsName2() {
+        compareExpectedToResult("42: Syntactic error next to 'piiins'" + System.lineSeparator(),
+                pathSyntacticErrors + "wrongPinsName2.moore");
     }
 
-    public void testWrongFunctionName() throws Exception {
-        assertEquals("48: Syntactic error next to 'fanction'" + System.lineSeparator(),
-                SyntacticAnalyzer.analyzeFile(pathSyntacticErrors + "wrongFunctionName.moore"));
+    @Test
+    public void wrongFunctionName() {
+        compareExpectedToResult("48: Syntactic error next to 'fanction'" + System.lineSeparator(),
+                pathSyntacticErrors + "wrongFunctionName.moore");
     }
 
-    public void testNotEofAfterMoore() throws Exception {
-        assertEquals("66: Syntactic error next to 'aaaaa'" + System.lineSeparator(),
-                SyntacticAnalyzer.analyzeFile(pathSyntacticErrors + "notEofAfterMoore.moore"));
+    @Test
+    public void notEofAfterMoore() {
+        compareExpectedToResult("66: Syntactic error next to 'aaaaa'" + System.lineSeparator(),
+                pathSyntacticErrors + "notEofAfterMoore.moore");
     }
 
     /* Lexical Errors */
-    public void testInvalidStateName() throws Exception {
-        assertEquals("4: Lexical Error: q - invalid symbol" + System.lineSeparator(),
-                SyntacticAnalyzer.analyzeFile(pathLexicalErrors + "invalidStateName.moore"));
+    @Test
+    public void invalidStateName() {
+        compareExpectedToResult("4: Lexical Error: q - invalid symbol" + System.lineSeparator(),
+                pathLexicalErrors + "invalidStateName.moore");
     }
 
-    public void testInvalidPinName() throws Exception {
-        assertEquals("11: Lexical Error: _ - invalid symbol" + System.lineSeparator(),
-                SyntacticAnalyzer.analyzeFile(pathLexicalErrors + "invalidPinName.moore"));
+    @Test
+    public void invalidPinName() {
+        compareExpectedToResult("11: Lexical Error: _ - invalid symbol" + System.lineSeparator(),
+                pathLexicalErrors + "invalidPinName.moore");
     }
 
-    public void testInvalidArrow() throws Exception {
-        assertEquals("19: Lexical Error: - - invalid symbol" + System.lineSeparator(),
-                SyntacticAnalyzer.analyzeFile(pathLexicalErrors + "invalidArrow.moore"));
+    @Test
+    public void invalidArrow() {
+        compareExpectedToResult("19: Lexical Error: - - invalid symbol" + System.lineSeparator(),
+                pathLexicalErrors + "invalidArrow.moore");
     }
 }
