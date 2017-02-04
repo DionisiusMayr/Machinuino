2e3ead19d9624d18b7550b15f4242e69dc861091
Daniel <d.willian_sc@hotmail.com>
2017-02-04 17:45:50 -0200

    Added transition function to CodeGenerator

--- src/main/java/Machinuino/CodeGenerator.java
+++ src/main/java/Machinuino/CodeGenerator.java
@@ -1,18 +1,21 @@
 package Machinuino;
 
+import Machinuino.model.BoolPin;
 import Machinuino.model.MooreMachine;
 import Machinuino.model.Pin;
+import Machinuino.model.Transition;
 
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class CodeGenerator {
 
     private static CodeGenerator instance;
+    private static String PIN_START_SYMBOL = "_";
+    private static String STATE_START_SYMBOL = "__";
 
     private CodeGenerator() {
     }
@@ -23,10 +26,11 @@ public class CodeGenerator {
     }
 
     public String generateCode(MooreMachine machine) {
-        StringBuilder builder = new StringBuilder();
-        builder.append(definePinsAndState(machine));
-
-        return builder.toString();
+        return definePinsAndState(machine) +
+                System.lineSeparator() +
+                defineIsHighFunction() +
+                System.lineSeparator() +
+                defineTransitionFunction(machine);
     }
 
     private String definePinsAndState(MooreMachine machine) {
@@ -55,7 +59,7 @@ public class CodeGenerator {
                 .append(";")
                 .append(System.lineSeparator());
 
-        builder.append(clockPinOrNot.get(Boolean.FALSE).stream());
+        builder.append(definePins(clockPinOrNot.get(Boolean.FALSE).stream()));
 
         return builder.toString();
     }
@@ -70,39 +74,98 @@ public class CodeGenerator {
 
     private String definePins(Stream<Pin> pinStream) {
         StringBuilder builder = new StringBuilder();
+        String lineSeparator = System.lineSeparator();
         for (Pin pin : pinStream.collect(Collectors.toSet())) {
-            builder.append("const int _")
+            builder.append("const int ")
+                    .append(PIN_START_SYMBOL)
                     .append(pin.getName())
                     .append(" = ")
                     .append(pin.getNumber())
                     .append(";")
-                    .append(System.lineSeparator());
+                    .append(lineSeparator);
         }
         return builder.toString();
     }
 
     private String defineStates(MooreMachine machine) {
         StringBuilder builder = new StringBuilder();
-        final String initialState = machine.getInitialState();
+        String lineSeparator = System.lineSeparator();
         builder.append("/* States */")
-                .append(System.lineSeparator())
-                .append("const int __")
-                .append(initialState)
-                .append(" = 0;")
-                .append(System.lineSeparator());
-        Set<String> statesWithoutTheInitial = machine.getStates()
-                .filter(state -> !state.equals(initialState))
-                .collect(Collectors.toSet());
-        int i = 1;
-        for (String state : statesWithoutTheInitial) {
-            builder.append("/* States */")
-                    .append("const int __")
+                .append(lineSeparator);
+        int i = 0;
+        for (String state : machine.getStates().collect(Collectors.toSet())) {
+            builder.append("const int ")
+                    .append(STATE_START_SYMBOL)
                     .append(state)
                     .append(" = ")
                     .append(i)
                     .append(";")
-                    .append(System.lineSeparator());
+                    .append(lineSeparator);
+            i++;
         }
         return builder.toString();
+    }
+
+    private String defineIsHighFunction() {
+        return "bool isHigh(int pin) {" + System.lineSeparator() +
+                "    return digitalRead(pin) == HIGH ? true : false;" + System.lineSeparator() +
+                "}" + System.lineSeparator();
+    }
+
+    private String defineTransitionFunction(MooreMachine machine) {
+        StringBuilder builder = new StringBuilder();
+        String lineSeparator = System.lineSeparator();
+        builder.append("int transition(int current) {").append(lineSeparator)
+                .append("    switch(current) {").append(lineSeparator)
+                .append(defineTransitionsOfEachState(machine))
+                .append("        default:").append(lineSeparator)
+                .append("            // Not reachable").append(lineSeparator)
+                .append("            exit(1);").append(lineSeparator)
+                .append("            break;").append(lineSeparator)
+                .append("    }").append(lineSeparator)
+                .append("}").append(lineSeparator);
+
+        return builder.toString();
+    }
+
+    private String defineTransitionsOfEachState(MooreMachine machine) {
+        StringBuilder builder = new StringBuilder();
+        String lineSeparator = System.lineSeparator();
+        for (String state : machine.getStates().sorted(String::compareToIgnoreCase)
+                .collect(Collectors.toSet())) {
+            builder.append("        case ")
+                    .append(STATE_START_SYMBOL)
+                    .append(state)
+                    .append(":").append(lineSeparator)
+                    .append(defineEachTransitionOfAState(state, machine))
+                    .append("            break;").append(lineSeparator);
+        }
+
+        return builder.toString();
+    }
+
+    private String defineEachTransitionOfAState(String state, MooreMachine machine) {
+        StringBuilder builder = new StringBuilder();
+        String lineSeparator = System.lineSeparator();
+        Set<Transition> transitions = machine.getTransitions()
+                .filter(transition -> transition.getPreviousState().equals(state))
+                .collect(Collectors.toSet());
+        for (Transition transition : transitions) {
+            builder.append("            if (");
+            String logicalExpression = transition.getInput().stream()
+                    .map(this::mapBoolPinToIsHigh)
+                    .collect(Collectors.joining(" && "));
+            builder.append(logicalExpression)
+                    .append(") return ")
+                    .append(STATE_START_SYMBOL)
+                    .append(transition.getNextState())
+                    .append(";").append(lineSeparator);
+        }
+        return builder.toString();
+    }
+
+    private String mapBoolPinToIsHigh(BoolPin boolPin) {
+        return (boolPin.isHigh() ? "" : "!") + "isHigh(" + PIN_START_SYMBOL +
+                boolPin.getPin().getName() + ")";
     }
 }
